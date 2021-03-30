9071c9bb19de7394322c83f03994e289f191a758
bodaraharshil <developer.harshilbodara@gmail.com>
2021-03-30 22:24:27 +0530

    created a form & validation

--- app/Http/Controllers/Users.php
+++ app/Http/Controllers/Users.php
@@ -8,6 +8,11 @@ class Users extends Controller
 {
     public function submit(Request $req)
     {
+        $req->validate([
+            "user"=>"required",
+            "phone"=>"required || min:8",
+            "password"=>"required || min:4 || min:8"
+        ]);
         print_r($req->input());
     }
 }
--- resources/views/User.blade.php
+++ resources/views/User.blade.php
@@ -3,10 +3,33 @@
         <title>Form tutorial </title>
     </head>
     <body>
+        <!-- <div>
+            <ul>
+                @foreach($errors->all() as $e)
+                <li>{{$e}}</li>
+                @endforeach
+            </ul>
+        </div> -->
         <form action="userdata" method="POST">
-        @csrf    
-        <input type="text" name="user" placeholder="Name"><br><br>
+        @csrf       
+            <input type="text" name="user" placeholder="Name"><br><br>
+            @error('user')
+            <div>
+                {{$message}}
+            </div>
+            @enderror
+            <input type="number" name="phone" placeholder="Phone"><br><br>
+            @error('phone')
+            <div>
+                {{$message}}
+            </div>
+            @enderror
             <input type="password" name="password" placeholder="Password"><br><br>
+            @error('password')
+            <div>
+                {{$message}}
+            </div>
+            @enderror
             <button type="submit">Submit</button>
         </form>
     </body>

5589628640bc3365e2dbe18ebbadfcdf6bf8f332
bodaraharshil <developer.harshilbodara@gmail.com>
2021-03-30 23:01:22 +0530

    added functonality for flash session

--- app/Http/Controllers/Users.php
+++ app/Http/Controllers/Users.php
@@ -8,11 +8,13 @@ class Users extends Controller
 {
     public function submit(Request $req)
     {
-        $req->validate([
-            "user"=>"required",
-            "phone"=>"required || min:8",
-            "password"=>"required || min:4 || min:8"
-        ]);
-        print_r($req->input());
+        // $req->validate([
+        //     "user"=>"required",
+        //     "phone"=>"required || min:8",
+        //     "password"=>"required || min:4 || min:8"
+        // ]);
+        // print_r($req->input());
+        $req->session()->flash('data','data has been submitted');
+        return redirect("/");
     }
 }

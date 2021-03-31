321c5215ea5a286c8c45f5c21dfa76320956b7dd
bodaraharshil <developer.harshilbodara@gmail.com>
2021-03-31 21:01:50 +0530

    created a insert,delete & update recored

--- app/Http/Controllers/Users.php
+++ app/Http/Controllers/Users.php
@@ -21,7 +21,42 @@ class Users extends Controller
 
     public function dbCheck()
     {
-            return DB::select("select * from migrations");
+            // return DB::table('users')
+            // ->where('name','demo')
+            // ->get();
+            
+            // return DB::table('users')->count();
+
+            // return DB::table('users')
+            // ->insert([
+            //     'name'=>'sam',
+            //     'email'=>'sam@gmail.com',
+            //     'password'=>"123456"
+            // ]);
+
+            // return DB::table('users')
+            // ->where('name','test')
+            // ->update([
+            //     'email'=>'test@gmail.com',
+            // ]);
+
+            // return DB::table('users')
+            // ->where('name','test')
+            // ->delete();
+
+            // return DB::table('users')
+            // ->sum('id');
+
+            // return DB::table('users')
+            // ->max('id');
+            
+            // return DB::table('users')
+            // ->min('id');
+
+            return DB::table('users')
+            ->avg('id');
+
+            // return DB::select("select * from users");
     //     $users = DB::select("select * from migrations");
     //     print_r($users);
     }
